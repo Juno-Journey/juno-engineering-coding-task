1e07610382d7cb0e5765ec27e342898784850531
Alexander Papashvili <sandropapashvili@gmail.com>
2022-04-18 20:17:07 +0400

    coomit

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,63 @@
-import React from "react";
-import { fetchImageUrls } from "../api/index";
+import React, { useState, useEffect } from 'react'
+
+import './styles.css'
+
+import { fetchImageUrls } from '../api/index'
 
 const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
-};
-export default ImageCarousel;
+    const [current, setCurrent] = useState(0)
+    const [images, setImages] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    fetchImageUrls().then((data) => setImages(data))
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? images.length - 1 : current - 1)
+    }
+
+    const nextSlide = () => {
+        setCurrent(current === images.length - 1 ? 0 : current + 1)
+    }
+
+    useEffect(() => {
+        setLoading(true)
+        fetchImageUrls()
+            .then((res) => setImages(res))
+            .finally(() => setLoading(false))
+    }, [])
+
+    return (
+        <section>
+            <div className='carousel'>
+                {loading ? (
+                    <h1>Wait a second...</h1>
+                ) : (
+                    <>
+                        {!images?.length ? (
+                            <h1>Sorry, there is no images</h1>
+                        ) : (
+                            <>
+                                <button className='carousel-button prev' onClick={() => prevSlide()}>
+                                    &#8592;
+                                </button>
+                                <button className='carousel-button next' onClick={() => nextSlide()}>
+                                    &#8594;
+                                </button>
+                                <ul>
+                                    {images?.map((image, index) => {
+                                        return (
+                                            <li key={index} className={`slide ${index === current && 'active-slide'}`}>
+                                                <img className='img' src={image} alt='juno' />
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </>
+                        )}
+                    </>
+                )}
+            </div>
+        </section>
+    )
+}
+export default ImageCarousel
--- src/task2/ecommerce.js
+++ src/task2/ecommerce.js
@@ -1,42 +1,75 @@
 ////////////////////////////////////////////// Helper code, do not edit /////////////////////////////////////////
-import { allIds, fetchOrderById } from "../api";
+import { allIds, fetchOrderById } from '../api'
 
 ////////////////////////////////// Your code tasks is below //////////////////////////////////////////////////////
 
-const fetchAllOrders = () => {
-    const ids = allIds;
+const fetchAllOrders = async () => {
+    const ids = allIds
     // .....
     //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
-};
 
-const bucketOrdersByUsers = () => {
-    let ordersByUsers = {};
+    const orders = await Promise.all(
+        ids.map(async (id) => {
+            const order = await fetchOrderById(id)
+            return order
+        })
+    )
+
+    return orders
+}
+
+const bucketOrdersByUsers = async () => {
+    let ordersByUsers = {}
     //   2. TODO: using the function from section 1 you should now bucket the orders by user.
     // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
-    return ordersByUsers;
-};
+    const orders = await fetchAllOrders()
 
-const getLast2WeeksOrders = () => {
+    orders.map((order) => {
+        return (ordersByUsers = {
+            ...ordersByUsers,
+            [order.userId]: ordersByUsers[order.userId] ? [...ordersByUsers[order.userId], order] : [order],
+        })
+    })
+
+    return ordersByUsers
+}
+
+const getLast2WeeksOrders = async () => {
     //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
-};
+    const orders = await fetchAllOrders()
+    const twoWeeksAgo = new Date(Date.now() - 12096e5)
+    const lastOrders = []
 
-const bucketOrdersByDate = () => {
-    let ordersByDate = {};
+    orders.forEach((order) => {
+        if (order.timestamp >= twoWeeksAgo) return [...lastOrders, order]
+    })
+
+    return lastOrders
+}
+
+const bucketOrdersByDate = async () => {
+    let ordersByDate = {}
     //   4. TODO: using the function from section 3 bucket the orders by date.
     // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
-    return ordersByDate;
-};
 
-fetchAllOrders();
+    const orders = await getLast2WeeksOrders()
+    ordersByDate.orders = orders.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date)
+    })
+
+    return ordersByDate
+}
+
+fetchAllOrders()
 // .then(console.log);
 
-bucketOrdersByUsers();
+bucketOrdersByUsers()
 // .then(console.log);
 
-getLast2WeeksOrders();
+getLast2WeeksOrders()
 // .then(console.log);
 
-bucketOrdersByDate();
+bucketOrdersByDate()
 // .then(console.log);
 
 ////////////////////////////////////////
